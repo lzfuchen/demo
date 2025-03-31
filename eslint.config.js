504c82d5fb620fb5454112619ce1ded1bcec360e
chen.fu <284992984@qq.com>
2025-03-31 16:01:04 +0800

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-]);
+  { ignores: ['*.d.ts', '**/dist'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx,vue}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...pluginVue.configs['flat/recommended']],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: { parser: tseslint.parser }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+  eslintConfigPrettier
+])
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,3 +1,7 @@
 <template>
-  <div class="mt-10">dashboard</div>
+  <div class="mt-10" :style="{ width: '100px' }" @click="test">dashboard</div>
 </template>
+
+<script setup lang="ts">
+function test() {}
+</script>
